d63a242c73c26347e900fdcf1096bd3317bbf73e
kuche1 <znamsiparolata4esno@abv.bg>
2023-09-17 00:31:32 +0300

    update

--- cubic.py
+++ cubic.py
@@ -1,5 +1,167 @@
 #! /usr/bin/env python3
 
+####################################### solve2
+
+import copy
+
+class Const:
+    def __init__(s, name, power):
+        assert type(name) == str
+        assert type(power) in [int, float]
+        s.name = name
+        s.power = power
+    def __repr__(s):
+        return f'{s.name}**{s.power}'
+
+class Term:
+    def __init__(s, mul, consts_top, consts_bot):
+        assert type(mul) in [int, float]
+        assert type(consts_top) == set
+        assert type(consts_bot) == set
+        s.mul = mul
+        s.consts_top = consts_top
+        s.consts_bot = consts_bot
+
+    def __repr__(s):
+        ret = f'({s.mul}'
+        for const in s.consts_top:
+            ret += f'*{const}'
+        ret += ')/('
+        for const in s.consts_bot:
+            ret += f'*{const}'
+        ret += ')'
+
+        if ret.endswith('/()'):
+            ret = ret[1:]
+            ret = ret[:-4]
+
+        return ret
+
+    def __truediv__(s, oth):
+        term = copy.deepcopy(s)
+        if type(oth) == Term:
+            term_div = oth
+            assert len(term_div.consts_top) == len(term_div.consts_bot) == 0, 'not implemented'
+            term.mul /= term_div.mul
+        else:
+            assert False, 'not implemented'
+        return term
+
+#     def __add__(s, oth):
+#         eq = Equasion(s)
+#         eq += oth
+#         return eq
+#     def comp(s, oth): # compatible
+#         return (s.con == oth.con and s.pow == oth.pow) or (s.pow == oth.pow == 0)
+
+class Equasion:
+    def __init__(s, terms):
+        s.terms = terms
+    
+    def __repr__(s):
+        ret = ''
+        for term in s.terms:
+            ret += f' + {term}'
+
+        if ret.startswith(' + '):
+            ret = ret[3:]
+
+        return ret
+
+    def __truediv__(s, oth):
+        eq = s
+        #print('+++ 1', eq)
+        if type(oth) == Term:
+            term_div = oth
+            for idx, term in enumerate(eq.terms):
+                #print('+++ 2', term)
+                eq.terms[idx] = term / term_div
+                #print('+++ 3', term)
+        else:
+            assert False, 'not implemented'
+        #print('+++ 4', eq)
+        return eq
+
+    def replace_const(s, name, new_term):
+        assert type(name) == str
+        assert type(new_term) == Term, 'not implemented'
+        for term_idx, term in enumerate(terms):
+            for const_idx, const in enumerate(term.consts_top): # TODO repeat for bot
+                if const.name == name:
+                    new_eq = solve2eq_a_plus_b_pow(new_term, const.power)
+                    TODO
+
+#     def __add__(s, new_term):
+#         eq = copy.deepcopy(s)
+#         for term in eq.terms:
+#             if term.comp(new_term):
+#                 term.val += new_term.val
+#                 return
+#         eq.terms.append(new_term)
+#         return eq
+
+def solve2_pow(a, pow):
+    assert type(a) == Term
+    assert type(pow) in [int, float]
+
+    if pow == 1:
+        TODO
+    
+    elif pow == 2:
+        TODO
+
+    else:
+        assert False, 'not implemented'
+
+def solve2_p2_p0(a, b):
+    # a*z**2 + b = 0
+    # a*z**2 = -b
+    # z**2 = -b/a
+    # z = +- (-b/a)**0.5
+    z1 = (-b/a)**0.5
+    z2 = -z1
+    return {z1, z2}
+
+def solve2_p2_p1_p0(a, b, c):
+
+    # a*z**2 + b*z + c = 0
+
+    a = Term(a, {Const('z', 2)}, set())
+    b = Term(b, {Const('z', 1)}, set())
+    c = Term(c, set(),           set())
+
+    eq = Equasion([a, b, c])
+
+    # z**2 + b*z + c = 0
+    eq /= Term(a.mul, set(), set())
+    
+    # z = y + d
+    #
+    # (y+d)**2 + b*(y+d) + c = 0
+    eq.replace_const('z', Term(1, {Const('w', 1), Const('f', 1)}, set()))
+    # y**2 + 2yd + d**2 + by + bd + c = 0
+    #
+    # 2yd + by = 0
+    # 2d + b = 0
+    # 2d = -b
+    # d = -b/2
+    #
+    # y**2 + d**2 + bd + c = 0
+    # y**2 + (-b/2)**2 + (-b/2)*d + c = 0
+    # y**2 + b**2/4 -bd/2 + c = 0
+    # y**2 + b**2/4 - 2bd/4 + c = 0
+    # y**2 + (b**2-2bd)/4 + c = 0
+    # y**2 = - (b**2-2bd)/4 - c
+
+    print(eq)
+
+solve2_p2_p1_p0(5, 4, 3)
+
+import sys
+sys.exit(69)
+
+####################################### solve1
+
 def solve_quadratic(a, b, c):
     # z^2*a + z^1*b + z^0*c = 0
 
@@ -177,6 +339,19 @@ def solve_cubic(original_a, original_b, original_c, original_d):
     x6 = y6 + e
 
     return [x1, x2, x3, x4, x5, x6]
+
+# def solve_4ic(a, b, c, d, e):
+
+#     # a*z^4 + b*z^3 + c*z^2 + d*z + e = 0
+
+#     # div by a
+#     b /= a
+#     c /= a
+#     d /= a
+#     e /= a
+#     a = 1
+
+#     # z^4 + b*z^3 + c*z^2 + d*z + e = 0
 
 
 print('a*x^3 + b*x^2 + c*x + d = 0')
